e0658b308942e1e1026a22f1f665783717f82534
SyedBasitYar <basityar6@gmail.com>
2024-06-15 01:43:47 +0530

    impl for reverse right angle triangle by different approach

--- src/main/java/com/learning/pattern/ReverseRightAngleTriangle.java
+++ src/main/java/com/learning/pattern/ReverseRightAngleTriangle.java
@@ -10,12 +10,24 @@ public class ReverseRightAngleTriangle {
 //          * * * *
 //        * * * * *
 
-        for(int i=1; i<=5; i++) {
-            for(int j=1; j<=5; j++) {
-                if(i+j>5)
-                    System.out.print("*");
-                else
-                    System.out.print(" ");
+//        for(int i=1; i<=5; i++) {
+//            for(int j=1; j<=5; j++) {
+//                if(i+j>5)
+//                    System.out.print("*");
+//                else
+//                    System.out.print(" ");
+//            }
+//            System.out.println();
+//        }
+
+        int n = 5;
+        for (int i= 1; i<=n; i++) {
+            for (int j=i; j<=n; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j=1; j<=i; j++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
