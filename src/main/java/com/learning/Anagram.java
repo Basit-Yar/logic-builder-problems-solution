c3e6d3c0b3acde35780fca4d9f191de3b44e79e0
SyedBasitYar <basityar6@gmail.com>
2024-07-01 23:34:56 +0530

    WIP for checking : given strings are Anagram or not

---
+++ src/main/java/com/learning/Anagram.java
@@ -0,0 +1,17 @@
+package com.learning;
+
+public class Anagram {
+    public static void main(String[] args) {
+        String string1 = "silent";
+        String string2 = "listen";
+
+        boolean isAnagram = isAnagram(string1, string2);
+    }
+
+    public static boolean isAnagram(String str1, String str2) {
+
+
+
+        return false;
+    }
+}
