6fe71471e98a72801f3849c6275f432e96c65ccb
SyedBasitYar <basityar6@gmail.com>
2024-10-22 01:06:58 +0530

    impl feature to find the lowest runs.

--- src/main/java/com/learning/ipa/practice/Solution_01_02.java
+++ src/main/java/com/learning/ipa/practice/Solution_01_02.java
@@ -1,12 +1,25 @@
 package com.learning.ipa.practice;
+
+import java.util.Arrays;
+
 public class Solution_01_02 {
     public static void main(String[] args) {
 
     }
 
-    // TODO: 10/22/2024 : The method should return the least runs of the Player from an array of Player objects for the given playerType.
-    public static void findPlayerWithLowestRuns(Player[] players, String playerType) {
+    /**
+     * The method should return the least runs of the Player from an array of Player objects
+     * for the given playerType.
+     */
+    public static int findPlayerWithLowestRuns(Player[] players, String playerType) {
 
+        int lowestRuns = Arrays.stream(players)
+                .filter(player -> player.getPlayerType().equalsIgnoreCase(playerType))
+                .mapToInt(player -> player.getRuns())
+                .min()
+                .orElse(-1);
+
+        return lowestRuns;
     }
 
     // TODO: 10/22/2024 : The method should return the array of Player objects belonging to the matchType passed as input parameter in Descending order of playerID. 
