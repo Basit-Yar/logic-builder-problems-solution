ace33d177365c398d0fd28ffc366daa215fc4091
SyedBasitYar <basityar6@gmail.com>
2024-10-22 00:50:32 +0530

    add todo.

--- src/main/java/com/learning/ipa/practice/Solution_01_02.java
+++ src/main/java/com/learning/ipa/practice/Solution_01_02.java
@@ -3,4 +3,15 @@ public class Solution_01_02 {
     public static void main(String[] args) {
 
     }
+
+    // TODO: 10/22/2024 : The method should return the least runs of the Player from an array of Player objects for the given playerType.
+    public static void findPlayerWithLowestRuns(Player[] players, String playerType) {
+
+    }
+
+    // TODO: 10/22/2024 : The method should return the array of Player objects belonging to the matchType passed as input parameter in Descending order of playerID. 
+    public static void findPlayerByMatchType(Player[] players, String matchType) {
+        
+    }
+
 }
