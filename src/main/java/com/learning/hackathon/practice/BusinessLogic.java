8e2c5243405496eaf702bd5c63a9e6d875dc4603
SyedBasitYar <basityar6@gmail.com>
2024-10-27 16:50:56 +0530

    feature to implement.

---
+++ src/main/java/com/learning/hackathon/practice/BusinessLogic.java
@@ -0,0 +1,14 @@
+package com.learning.hackathon.practice;
+
+import java.util.List;
+
+public class BusinessLogic {
+
+    public List<Book> getBooksBelongingToAGenre(List<Book> books, String genre) {
+        return null;
+    }
+
+    public List<Book> calculateDiscountedPrice(List<Book> books, String genre, double discount) {
+        return null;
+    }
+}
