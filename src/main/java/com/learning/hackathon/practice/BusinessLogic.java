c1b5d2e6342f2e3286dd892d35eed896c47c8378
SyedBasitYar <basityar6@gmail.com>
2024-10-27 17:06:10 +0530

    get books belonging to a genre

--- src/main/java/com/learning/hackathon/practice/BusinessLogic.java
+++ src/main/java/com/learning/hackathon/practice/BusinessLogic.java
@@ -1,11 +1,25 @@
 package com.learning.hackathon.practice;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BusinessLogic {
 
+    /**
+     * It should find all the books belonging to the input genre and should
+     * return the list of books matching the genre.
+     *
+     * If no matching genre is found, print an error message
+     * "Genre is not available in the list"
+     *
+     * @param books
+     * @param genre
+     * @return
+     */
     public List<Book> getBooksBelongingToAGenre(List<Book> books, String genre) {
-        return null;
+        return books.stream()
+                .filter(book -> book.getGenre().equalsIgnoreCase(genre))
+                .collect(Collectors.toList());
     }
 
     public List<Book> calculateDiscountedPrice(List<Book> books, String genre, double discount) {
