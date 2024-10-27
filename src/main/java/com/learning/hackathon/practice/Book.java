8805f25e9e3d8a5e6978e8d5ab61a973734f291d
SyedBasitYar <basityar6@gmail.com>
2024-10-27 16:59:38 +0530

    generate getters, setters and constructor for entity class.

--- src/main/java/com/learning/hackathon/practice/Book.java
+++ src/main/java/com/learning/hackathon/practice/Book.java
@@ -7,4 +7,52 @@ public class Book {
     private String genre;
     private double price;
     private Author author;
+
+    public Book(Integer bookId, String title, String genre, double price, Author author) {
+        this.bookId = bookId;
+        this.title = title;
+        this.genre = genre;
+        this.price = price;
+        this.author = author;
+    }
+
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Integer bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
 }
